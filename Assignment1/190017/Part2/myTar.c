0f8da79745458bbce3b90f0cb870bb79ededff2b
Abhay Mishra <mishraabhay9179913420@gmail.com>
2021-08-31 22:08:28 +0530

    Part 2 cleaned

--- Assignment1/190017/Part2/myTar.c
+++ Assignment1/190017/Part2/myTar.c
@@ -7,6 +7,16 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+
+// utility function for parsing the path
+void moveToDirectory(char* path, char* lastName);
+
+// utility function for reading tar file
+void readTarFile(char* flag, int tarfd,int listfd,char* singleFile);
+
+// utility function for  preparing tarStructure file
+int prepareListStructure(int tarfd);
+
 int main(int argc, char **argv)
 {
 	// atleast 3 arguments
@@ -29,29 +39,11 @@ int main(int argc, char **argv)
 			exit(-1);
 		}
 
-		// assume the paths are seprated by '/'
-		char *str1, *token;
-		// relative path by default
-		// absolute path: argv[2] is NON-NULL
-		if (argv[2][0] == '/')
-		{
-			chdir("/");
-		}
-
-		// reach to the end
-		for (str1 = argv[2];; str1 = NULL)
-		{
-			token = strtok(str1, "/");
-			if (token == NULL)
-			{
-				break;
-			}
-			// move to the next directory
-			if (chdir(token) < 0)
-			{
-				printf("Failed to complete extraction operation\n");
-				exit(-1);
-			}
+		char lastDir[32] = ".";
+		moveToDirectory(argv[2],lastDir);
+		if(chdir(lastDir)<0){
+			printf("Failed to complete extraction operation\n");
+			exit(-1);
 		}
 
 		// underlying fd can be obtained using
@@ -171,33 +163,9 @@ int main(int argc, char **argv)
 
 		// change directory
 
-		// assume the paths are seprated by '/'
-		char *str1, *token;
 		// relative path by default
 		char lastName[32] = ".";
-		// absolute path: argv[2] is NON-NULL
-		if (argv[2][0] == '/')
-		{
-			lastName[0] = '/';
-		}
-
-		// reach to the end
-		for (str1 = argv[2];; str1 = NULL)
-		{
-			token = strtok(str1, "/");
-			if (token == NULL)
-			{
-				break;
-			}
-			// move to the next directory
-			if (chdir(lastName) < 0)
-			{
-				printf("Failed to complete extraction operation\n");
-				exit(-1);
-			}
-			// moving deeper
-			strcpy(lastName, token);
-		}
+		moveToDirectory(argv[2],lastName);
 
 		// Now in correct directory
 		// open tar file
@@ -228,97 +196,11 @@ int main(int argc, char **argv)
 			exit(-1);
 		}
 
-		// get total no of files
-		char sfileCount[32];
 
-		// set offset to the last 32 bytes
-		if (lseek(tarfd, -32, SEEK_END) < 0)
-		{
-			printf("Failed to complete extraction operation\n");
-			exit(-1);
-		}
+		// read tar file
+		readTarFile(argv[1],tarfd,-1,NULL);
 
-		// read exactly 32 bytes
-		if (read(tarfd, sfileCount, 32) != 32)
-		{
-			printf("Failed to complete extraction operation\n");
-			exit(-1);
-		}
-		if (lseek(tarfd, 0, SEEK_SET) < 0)
-		{
-			printf("Failed to complete extraction operation\n");
-			exit(-1);
-		}
-
-		// no of files to be extracted
-		int fileCount = atoi(sfileCount);
-
-		// extract files one by one from tar file
-		for (int i = 0; i < fileCount; i++)
-		{
-			char fileName[32];
-			char sfileSize[32];
-
-			// read exactly 32 bytes
-			if (read(tarfd, fileName, 32) != 32)
-			{
-				printf("Failed to complete extraction operation\n");
-				exit(-1);
-			}
-
-			// read exactly 32 bytes
-			if (read(tarfd, sfileSize, 32) != 32)
-			{
-				printf("Failed to complete extraction operation\n");
-				exit(-1);
-			}
-
-			// size of file to be created
-			unsigned long long fileSize = atol(sfileSize);
-
-			// File to be extracted in this iteration
-			int newTar = open(fileName, O_CREAT | O_RDWR, 0644);
-
-			if (newTar < 0)
-			{
-				printf("Failed to complete extraction operation\n");
-				exit(-1);
-			}
-			while (fileSize > 0)
-			{
-				int remBytes = 32;
-				// file size need not to be a multiple of 32
-				if (fileSize < 32)
-				{
-					remBytes = fileSize;
-				}
-				char buff[remBytes];
-				if (read(tarfd, buff, remBytes) < 0)
-				{
-					printf("Failed to complete extraction operation\n");
-					exit(-1);
-				}
-				if (write(newTar, buff, remBytes) < 0)
-				{
-					printf("Failed to complete extraction operation\n");
-					exit(-1);
-				}
-
-				// update file size
-				fileSize -= remBytes;
-			}
-
-			// file extracted from tar
-			if (close(newTar) < 0)
-			{
-				printf("Failed to complete extraction operation\n");
-				exit(-1);
-			}
-
-			// offset of tarfd is at starting of next file name
-		}
-		// file extracted
-		close(tarfd);
+		// done
 	}
 	else if (strcmp(flag, "-e") == 0)
 	{
@@ -332,33 +214,9 @@ int main(int argc, char **argv)
 			exit(-1);
 		}
 
-		// assume the paths are seprated by '/'
-		char *str1, *token;
 		// relative path by default
 		char lastName[32] = ".";
-		// absolute path: argv[2] is NON-NULL
-		if (argv[2][0] == '/')
-		{
-			lastName[0] = '/';
-		}
-
-		// reach to the end
-		for (str1 = argv[2];; str1 = NULL)
-		{
-			token = strtok(str1, "/");
-			if (token == NULL)
-			{
-				break;
-			}
-			// move to the next directory
-			if (chdir(lastName) < 0)
-			{
-				printf("Failed to complete extraction operation\n");
-				exit(-1);
-			}
-			// moving deeper
-			strcpy(lastName, token);
-		}
+		moveToDirectory(argv[2],lastName);
 
 		// open tar file, now in correct directory
 		int tarfd = open(lastName, O_RDONLY);
@@ -385,99 +243,10 @@ int main(int argc, char **argv)
 			exit(-1);
 		}
 
-		// get total no of files
-		char sfileCount[32];
+		// read tar file
 
-		// set offset to the last 32 bytes
-		if (lseek(tarfd, -32, SEEK_END) < 0)
-		{
-			printf("Failed to complete extraction operation\n");
-			exit(-1);
-		}
+		readTarFile(argv[1],tarfd,-1,argv[3]);
 
-		// read exactly 32 bytes
-		if (read(tarfd, sfileCount, 32) != 32)
-		{
-			printf("Failed to complete extraction operation\n");
-			exit(-1);
-		}
-		if (lseek(tarfd, 0, SEEK_SET) < 0)
-		{
-			printf("Failed to complete extraction operation\n");
-			exit(-1);
-		}
-
-		// no of files to be extracted
-		int fileCount = atoi(sfileCount);
-
-		// extract files one by one from tar file
-		for (int i = 0; i < fileCount; i++)
-		{
-			char fileName[32];
-			char sfileSize[32];
-
-			// read exactly 32 bytes
-			if (read(tarfd, fileName, 32) != 32)
-			{
-				printf("Failed to complete extraction operation\n");
-				exit(-1);
-			}
-
-			// read exactly 32 bytes
-			if (read(tarfd, sfileSize, 32) != 32)
-			{
-				printf("Failed to complete extraction operation\n");
-				exit(-1);
-			}
-
-			// size of file to be created
-			unsigned long long fileSize = atol(sfileSize);
-
-			// check if this is the file we are after
-			if (strcmp(fileName, argv[3]) != 0)
-			{
-				// move to the next file
-				lseek(tarfd, fileSize, SEEK_CUR);
-				continue;
-			}
-
-			// File to be extracted in this iteration
-			int newTar = open(fileName, O_CREAT | O_RDWR, 0644);
-			while (fileSize > 0)
-			{
-				int remBytes = 32;
-				// file size need not to be a multiple of 32
-				if (fileSize < 32)
-				{
-					remBytes = fileSize;
-				}
-				char buff[remBytes];
-				if (read(tarfd, buff, remBytes) < 0)
-				{
-					printf("Failed to complete extraction operation\n");
-					exit(-1);
-				}
-				if (write(newTar, buff, remBytes) < 0)
-				{
-					printf("Failed to complete extraction operation\n");
-					exit(-1);
-				}
-
-				// update file size
-				fileSize -= remBytes;
-			}
-
-			// file extracted from tar
-			if (close(newTar) < 0)
-			{
-				printf("Failed to complete extraction operation\n");
-				exit(-1);
-			}
-
-			// file extracted
-			close(tarfd);
-			return 0;
-		}
 
 		// if reached here means file not found
 		printf("No such file is present in tar file\n");
@@ -495,33 +264,10 @@ int main(int argc, char **argv)
 			exit(-1);
 		}
 
-		// assume the paths are seprated by '/'
-		char *str1, *token;
 		// relative path by default
 		char lastName[32] = ".";
-		// absolute path: argv[2] is NON-NULL
-		if (argv[2][0] == '/')
-		{
-			lastName[0] = '/';
-		}
+		moveToDirectory(argv[2],lastName);
 
-		// reach to the end
-		for (str1 = argv[2];; str1 = NULL)
-		{
-			token = strtok(str1, "/");
-			if (token == NULL)
-			{
-				break;
-			}
-			// move to the next directory
-			if (chdir(lastName) < 0)
-			{
-				printf("Failed to complete extraction operation\n");
-				exit(-1);
-			}
-			// moving deeper
-			strcpy(lastName, token);
-		}
 		// open tar file, now in correct directory
 		int tarfd = open(lastName, O_RDONLY);
 
@@ -532,87 +278,103 @@ int main(int argc, char **argv)
 		}
 
 		// Prepare for listing
-		int listfd = open("tarStructure", O_CREAT | O_RDWR, 0644);
-		if (listfd < 0)
+		int listfd = prepareListStructure(tarfd);
+
+		// read file
+		readTarFile(argv[1],tarfd,listfd,NULL);
+
+		// done
+	}
+	else
+	{
+		printf("Invalid arguments\n");
+		exit(-1);
+	}
+
+	return 0;
+}
+void moveToDirectory(char* path, char* lastName){
+	// assume the paths are seprated by '/'
+	char *str1, *token;
+	// absolute path: argv[2] is NON-NULL
+	if (path[0] == '/')
+	{
+		lastName[0] = '/';
+	}
+
+	// reach to the end
+	for (str1 = path;;str1 = NULL)
+	{
+		token = strtok(str1, "/");
+		if (token == NULL)
+		{
+			break;
+		}
+		// move to the next directory
+		if (chdir(lastName) < 0)
 		{
 			printf("Failed to complete extraction operation\n");
 			exit(-1);
 		}
+		// moving deeper
+		strcpy(lastName, token);
+	}
 
-		unsigned long long tarSize = lseek(tarfd, 0, SEEK_END);
-		char starSize[32];
-		sprintf(starSize, "%lld\n", tarSize);
-		int byteCount = strlen(starSize);
+}
 
-		// write the size of tar file
-		if (write(listfd, starSize, byteCount) != byteCount)
-		{
-			printf("Failed to complete extraction operation");
-			exit(-1);
-		}
+void readTarFile(char* flag, int tarfd,int listfd,char* singleFile){
+	// get total no of files
+	char sfileCount[32];
 
-		// get total no of files
-		char sfileCount[32];
+	// set offset to the last 32 bytes
+	if (lseek(tarfd, -32, SEEK_END) < 0)
+	{
+		printf("Failed to complete extraction operation\n");
+		exit(-1);
+	}
 
-		// set offset to the last 32 bytes
-		if (lseek(tarfd, -32, SEEK_END) < 0)
+	// read exactly 32 bytes
+	if (read(tarfd, sfileCount, 32) != 32)
+	{
+		printf("Failed to complete extraction operation\n");
+		exit(-1);
+	}
+	if (lseek(tarfd, 0, SEEK_SET) < 0)
+	{
+		printf("Failed to complete extraction operation\n");
+		exit(-1);
+	}
+
+	// no of files to be extracted
+	int fileCount = atoi(sfileCount);
+
+	// extract files one by one from tar file
+	for (int i = 0; i < fileCount; i++)
+	{
+		char fileName[32] = {0};
+		char sfileSize[32] = {0};
+
+		// read exactly 32 bytes
+		if (read(tarfd, fileName, 32) != 32)
 		{
 			printf("Failed to complete extraction operation\n");
 			exit(-1);
 		}
 
 		// read exactly 32 bytes
-		if (read(tarfd, sfileCount, 32) != 32)
-		{
-			printf("Failed to complete extraction operation\n");
-			exit(-1);
-		}
-		// Move offset to start of file
-		if (lseek(tarfd, 0, SEEK_SET) < 0)
+		if (read(tarfd, sfileSize, 32) != 32)
 		{
 			printf("Failed to complete extraction operation\n");
 			exit(-1);
 		}
 
-		// no of files to be extracted
-		int fileCount = atoi(sfileCount);
+		// size of file to be created
+		unsigned long long fileSize = atol(sfileSize);
 
-		// write the no of files in tar archive
-		strcat(sfileCount, "\n");
-		byteCount = strlen(sfileCount);
-		if (write(listfd, sfileCount, byteCount) != byteCount)
-		{
-			printf("Failed to complete extraction operation\n");
-			exit(-1);
-		}
-
-		// extract files one by one from tar file
-		for (int i = 0; i < fileCount; i++)
-		{
-			char fileName[32];
-			char sfileSize[32];
-
-			// read exactly 32 bytes
-			if (read(tarfd, fileName, 32) != 32)
-			{
-				printf("Failed to complete extraction operation\n");
-				exit(-1);
-			}
-
-			// read exactly 32 bytes
-			if (read(tarfd, sfileSize, 32) != 32)
-			{
-				printf("Failed to complete extraction operation\n");
-				exit(-1);
-			}
-
-			// size of file to be created
-			unsigned long long fileSize = atol(sfileSize);
-
+		// listing only
+		if(strcmp(flag,"-l") == 0){
 			// write file name
-			strcat(fileName, " ");
-			byteCount = strlen(fileName);
-			if (write(listfd, fileName, byteCount) != byteCount)
+			if (write(listfd, fileName, strlen(fileName)) != strlen(fileName))
 			{
 				printf("Failed to complete extraction operation\n");
 				exit(-1);
@@ -620,8 +382,7 @@ int main(int argc, char **argv)
 
 			// write file size
 			strcat(sfileSize, "\n");
-			byteCount = strlen(sfileSize);
-			if (write(listfd, sfileSize, byteCount) != byteCount)
+			if (write(listfd, sfileSize, strlen(sfileSize)) != strlen(sfileSize))
 			{
 				printf("Failed to complete extraction operation\n");
 				exit(-1);
@@ -633,16 +394,125 @@ int main(int argc, char **argv)
 				printf("Failed to complete extraction operation\n");
 				exit(-1);
 			}
-			// offset of tarfd is at starting of next file name
+
+			// no need to create files
+			continue;
 		}
-		// file listed
-		close(tarfd);
+
+		if(strcmp(flag,"-e") == 0){
+			// check if this is not the file we are after
+			if (strcmp(fileName, singleFile) != 0)
+			{
+				// move to the next file
+				lseek(tarfd, fileSize, SEEK_CUR);
+				continue;
+			}
+		}
+
+		// File to be extracted in this iteration
+		int newTar = open(fileName, O_CREAT | O_RDWR, 0644);
+
+		if (newTar < 0)
+		{
+			printf("Failed to complete extraction operation\n");
+			exit(-1);
+		}
+		while (fileSize > 0)
+		{
+			int remBytes = 32;
+			// file size need not to be a multiple of 32
+			if (fileSize < 32)
+			{
+				remBytes = fileSize;
+			}
+			char buff[remBytes];
+			if (read(tarfd, buff, remBytes) < 0)
+			{
+				printf("Failed to complete extraction operation\n");
+				exit(-1);
+			}
+			if (write(newTar, buff, remBytes) < 0)
+			{
+				printf("Failed to complete extraction operation\n");
+				exit(-1);
+			}
+
+			// update file size
+			fileSize -= remBytes;
+		}
+
+		// file extracted from tar
+		if (close(newTar) < 0)
+		{
+			printf("Failed to complete extraction operation\n");
+			exit(-1);
+		}
+
+		if(strcmp(flag,"-e") == 0){
+			// done with single file
+			close(tarfd);
+		}
+		// offset of tarfd is at starting of next file name
 	}
-	else
+	// file extracted
+	close(tarfd);
+}
+
+int prepareListStructure(int tarfd){
+	// Prepare for listing
+	int listfd = open("tarStructure", O_CREAT | O_RDWR, 0644);
+	if (listfd < 0)
 	{
-		printf("Invalid arguments\n");
+		printf("Failed to complete extraction operation\n");
 		exit(-1);
 	}
 
-	return 0;
+	unsigned long long tarSize = lseek(tarfd, 0, SEEK_END);
+	char starSize[32];
+	sprintf(starSize, "%lld\n", tarSize);
+	int byteCount = strlen(starSize);
+
+	// write the size of tar file
+	if (write(listfd, starSize, byteCount) != byteCount)
+	{
+		printf("Failed to complete extraction operation");
+		exit(-1);
+	}
+
+	// get total no of files
+	char sfileCount[32];
+
+	// set offset to the last 32 bytes
+	if (lseek(tarfd, -32, SEEK_END) < 0)
+	{
+		printf("Failed to complete extraction operation\n");
+		exit(-1);
+	}
+
+	// read exactly 32 bytes
+	if (read(tarfd, sfileCount, 32) != 32)
+	{
+		printf("Failed to complete extraction operation\n");
+		exit(-1);
+	}
+	// Move offset to start of file
+	if (lseek(tarfd, 0, SEEK_SET) < 0)
+	{
+		printf("Failed to complete extraction operation\n");
+		exit(-1);
+	}
+
+	// no of files to be extracted
+	int fileCount = atoi(sfileCount);
+
+	// write the no of files in tar archive
+	strcat(sfileCount, "\n");
+	byteCount = strlen(sfileCount);
+	if (write(listfd, sfileCount, byteCount) != byteCount)
+	{
+		printf("Failed to complete extraction operation\n");
+		exit(-1);
+	}
+
+	return listfd;
 }
